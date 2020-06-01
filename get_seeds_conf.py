25188200fd3d8ad486230045d348c730fe066933
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-06-01 19:28:06 +0200

    Update get_seeds_conf.py

--- get_seeds_conf.py
+++ get_seeds_conf.py
@@ -30,3 +30,15 @@ TESTCASE_TOOLS_CONFIGS = [
         dev_test,
         shadow_for_seeds,
 ]
+
+old_build_func = CODE_BUILDER_FUNCTION
+def override_build(*args, **kwargs):
+    # Make sure that shadow don't generate tests, but just replay
+    # Remove the klee changes
+    if flags_list is None:
+        flags_list = [ "-DRESOLVE_KLEE_CHANGE=11" ]
+		else:	
+        flags_list.append("-DRESOLVE_KLEE_CHANGE=11")
+    return old_build_func(*args, **kwargs)
+  
+CODE_BUILDER_FUNCTION=override_build

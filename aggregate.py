073f4bbc2b0a7c085cb3712ebf7166e44ceedd1d
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-07-13 13:53:12 +0200

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -270,7 +270,8 @@ def main():
                 
         # plot Trend
         linesplotfile = os.path.join(outdir, "lineplot")
-        plotTrend(tech2time2fd, linesplotfile, xlabel="ellapsed time (s)", ylabel=ylabel, yticks_range=yticks_range, order=sorted(list(tech2time2fd)), x_as_xticks=True)
+        plotTrend(tech2time2fd, linesplotfile, xlabel="ellapsed time (s)", ylabel=ylabel, yticks_range=yticks_range, \
+                                                                            order=sorted(list(tech2time2fd), key=string_num_sort_key), x_as_xticks=True)
         
         # XXX: Update id2test2time by setting the time of tests without time to the maximum available value
         for b_id, test_set in list(id2bugtests.items()) + list(id2difftests.items()):
